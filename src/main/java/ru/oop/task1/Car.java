a9b11b69e7c6ce7764f7e78a3905868deb763139
Михаил Соломеин <solomein-m@mail.ru>
2025-10-17 13:09:05 +0500

    Task1 выполнена

---
+++ src/main/java/ru/oop/task1/Car.java
@@ -0,0 +1,13 @@
+package ru.oop.task1;
+
+/**
+ * Машина
+ */
+public class Car {
+    /**
+     * Перемещает person максимально близко к destination
+     */
+    public void driveTo(Person person, Position destination) {
+        //TODO
+    }
+}
