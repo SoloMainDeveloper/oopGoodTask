d1816e4af957fb29dcfcb244fc3754cf435af221
Михаил Соломеин <solomein-m@mail.ru>
2025-10-18 10:22:17 +0500

    Task2 выполнена

--- src/main/java/ru/oop/task2/Person.java
+++ src/main/java/ru/oop/task2/Person.java
@@ -14,4 +14,9 @@ interface Person extends Positioned {
      * @param destination место назначения
      */
     void walk(Position destination);
+
+    /**
+     * Выбирает транспорт для передвижения
+     */
+    Transport chooseTransport(Position destination);
 }
